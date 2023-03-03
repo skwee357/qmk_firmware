80b663a65737eca79cbe3c2c97dc32a6b8f9a6b4
skwee357 <ko.dmitri@gmail.com>
2023-03-03 19:18:15 +0200

    my corne layout

---
+++ keyboards/crkbd/keymaps/skwee357/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+CAPS_WORD_ENABLE = yes
+VIA_ENABLE = yes
