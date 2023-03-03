323eb6025512f077a446ba46a65cb44fcbdf3708
skwee357 <ko.dmitri@gmail.com>
2023-03-03 19:51:12 +0200

    change modifiers to one shot

--- keyboards/crkbd/keymaps/skwee357/keymap.c
+++ keyboards/crkbd/keymaps/skwee357/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 OSM(MOD_LSFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, OSM(MOD_RSFT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            LALT_T(KC_DEL),   MO(1), HYPR_T(KC_SPC),   RCMD_T(KC_ENT), MO(2), RCTL_T(KC_BSPC)
+                            LALT_T(KC_DEL),   OSL(1), HYPR_T(KC_SPC),   RCMD_T(KC_ENT), OSL(2), RCTL_T(KC_BSPC)
                                       //`--------------------------'  `--------------------------'
 
   ),
